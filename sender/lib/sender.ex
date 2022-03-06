77fe93e653d3384efd4a25de1a94862c4d4bee43
Eric Olson <eric.rob.olson@gmail.com>
2022-03-06 08:04:25 -0800

    Finished chapter 1

---
+++ sender/lib/sender.ex
@@ -0,0 +1,17 @@
+defmodule Sender do
+  def send_email("failure" = email) do
+    raise "Oops, #{email} not sent"
+  end
+
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email sent"}
+  end
+
+  def notify_all(emails) do
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
+    |> Enum.to_list()
+  end
+end
